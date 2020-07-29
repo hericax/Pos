16bce89f4f2b091cc64b232caea8fce91f6bd6f9
hericax <57000293+hericax@users.noreply.github.com>
2020-07-29 17:44:52 -0300

    versão - marco deleção
    inclusão de tratamento de dados faltantes antes do tratamento de cada feature.
    A partir daqui, irei excluir do código partes onde não aparecem mais registros duplicados.

--- curso_python_alura/python-Parte1e2-jogos/projeto - jogos/exemplo_print_format.py
+++
@@ -1,8 +0,0 @@
-dia_ini = 24
-dia_fim = 28
-mes = "fevereiro"
-ano = 2017
-
-print("Em ", ano, "o carnaval acontece em", mes, "do dia", dia_ini, "até o dia", dia_fim)
-
-print ("Em  {} o carnaval acontece em {} do dia {} até o dia {}".format(ano,  mes, dia_ini, dia_fim))
